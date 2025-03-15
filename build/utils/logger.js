a06ea7ec56a12283c2b47171665a5066e7678e35
Hydradevx <themaskedprogrammer1234@gmail.com>
2025-03-15 19:17:17 +0530

    Adding SOme Functionality

--- build/utils/logger.js
+++ build/utils/logger.js
@@ -9,9 +9,19 @@ exports.start = start;
 const fs_1 = __importDefault(require("fs"));
 const ansi_colors_1 = __importDefault(require("ansi-colors"));
 const path_1 = __importDefault(require("path"));
+const inquirer_1 = __importDefault(require("inquirer"));
 const PackagePath = path_1.default.join(__dirname, "../../package.json");
 const packageJson = fs_1.default.readFileSync(PackagePath);
 const version = JSON.parse(packageJson).version;
+let pageno = 1;
+function getPage(pageno) {
+  let page;
+  if (pageno === 1) {
+    page = `
+    ${ansi_colors_1.default.cyan("1")} - ${ansi_colors_1.default.green("IP Lookup")}`;
+  }
+  return page;
+}
 const banner = `
 ██╗  ██╗██╗   ██╗██████╗ ██████╗ ██╗ ██████╗ ███╗   ██╗
 ██║  ██║╚██╗ ██╔╝██╔══██╗██╔══██╗██║██╔═══██╗████╗  ██║
@@ -20,7 +30,50 @@ const banner = `
 ██║  ██║   ██║   ██████╔╝██║  ██║██║╚██████╔╝██║ ╚████║
 ╚═╝  ╚═╝   ╚═╝   ╚═════╝ ╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝     
 Discord Tool v${version}
+
+
+Input Commands:
+X - Exit the Tool
+> - Next Page
+< - Previous Page
 `;
+let currentpage = getPage(pageno);
 function start() {
-  console.log(ansi_colors_1.default.blue(banner));
+  console.clear();
+  console.log(ansi_colors_1.default.cyan(banner));
+  console.log(ansi_colors_1.default.cyanBright(currentpage));
+  prompt().then((answers) => {
+    const command = answers.command;
+    if (command === "X") {
+      process.exit();
+    } else if (command === ">") {
+      pageno++;
+      currentpage = getPage(pageno);
+      start();
+    } else if (command === "<") {
+      pageno--;
+      currentpage = getPage(pageno);
+      start();
+    } else if (isNumber(command)) {
+      const func = funcs[command];
+      if (func) {
+        require(func.path);
+      }
+    }
+  });
 }
+function isNumber(value) {
+  return !isNaN(parseInt(value));
+}
+function prompt() {
+  return inquirer_1.default.prompt([
+    {
+      name: "command",
+      message: ansi_colors_1.default.blue("What would you like to do?"),
+      type: "input",
+    },
+  ]);
+}
+const funcs = {
+  1: { name: "iplookup", path: "../funcs/iplookup" },
+};
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,23 @@
 import fs from "fs";
 import colors from "ansi-colors";
 import path from "path";
+import inquirer from "inquirer";
 
 const PackagePath = path.join(__dirname, "../../package.json");
 const packageJson: any = fs.readFileSync(PackagePath);
 const version = JSON.parse(packageJson).version;
+
+let pageno = 1;
+
+function getPage(pageno: number) {
+  let page;
+  if (pageno === 1) {
+    page = `
+    ${colors.cyan("1")} - ${colors.green("IP Lookup")}`;
+  }
+
+  return page;
+}
 
 const banner = `
 ██╗  ██╗██╗   ██╗██████╗ ██████╗ ██╗ ██████╗ ███╗   ██╗
@@ -14,8 +27,55 @@ const banner = `
 ██║  ██║   ██║   ██████╔╝██║  ██║██║╚██████╔╝██║ ╚████║
 ╚═╝  ╚═╝   ╚═╝   ╚═════╝ ╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝     
 Discord Tool v${version}
+
+
+Input Commands:
+X - Exit the Tool
+> - Next Page
+< - Previous Page
 `;
 
+let currentpage: any = getPage(pageno);
+
 export function start() {
-  console.log(colors.blue(banner));
+  console.clear();
+  console.log(colors.cyan(banner));
+  console.log(colors.cyanBright(currentpage));
+  prompt().then((answers) => {
+    const command = answers.command;
+    if (command === "X") {
+      process.exit();
+    } else if (command === ">") {
+      pageno++;
+      currentpage = getPage(pageno);
+      start();
+    } else if (command === "<") {
+      pageno--;
+      currentpage = getPage(pageno);
+      start();
+    } else if (isNumber(command)) {
+      const func = funcs[command];
+      if (func) {
+        require(func.path);
+      }
+    }
+  });
 }
+
+function isNumber(value: string): boolean {
+  return !isNaN(parseInt(value));
+}
+
+function prompt() {
+  return inquirer.prompt([
+    {
+      name: "command",
+      message: colors.blue("What would you like to do?"),
+      type: "input",
+    },
+  ]);
+}
+
+const funcs: any = {
+  1: { name: "iplookup", path: "../funcs/iplookup" },
+};
